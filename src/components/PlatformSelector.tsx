86d17cafad0de0d88ceb15b1ad724dc5b89b42b1
naingwin-aung <naingwinaung1710@gmail.com>
2024-10-08 21:42:30 +0700

    change platform style

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,23 +4,28 @@ import usePlatform from "../hooks/usePlatform";
 import { Platform } from "../hooks/useGames";
 
 interface Props {
-  onSelectPlatform: (platform: Platform) => void,
-  selectedPlatform: Platform | null
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({onSelectPlatform, selectedPlatform}: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatform();
 
-  if(error) return null;
+  if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name  || 'Platforms'}
+        Platform: {selectedPlatform?.name}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
