8cfdb8b3746afa33ae4e20ac0e772d96b6892d2f
naingwin-aung <naingwinaung1710@gmail.com>
2024-10-09 23:19:02 +0700

    ship genre with the app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -18,6 +18,7 @@ interface Props {
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
+  
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   if (error) return null;
